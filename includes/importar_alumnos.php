caba2f819c88f2e4560fb23c75bad32748c6fad5
billio <profebillio@gmail.com>
2019-12-19 14:17:31 -0600

    Arreglado

--- includes/importar_alumnos.php
+++ includes/importar_alumnos.php
@@ -1,33 +1,39 @@
 <?php include("menu.php");
 include("../conexion/conexion.php");
+
 if(isset($_POST["enviar"]))
 {
   $archivo = $_FILES["archivo"]["name"];
   $archivo_copia = $_FILES["archivo"]["tmp_name"];
   $archivo_guardado = "copia_".$archivo;
 
-$csv = explode(".",$archivo);
-  if($csv[1]<>"csv"){
-    echo("Por favor elija un archivo con extensión CSV");
-  }else{
-    $data=array();
-    $fila = 0;
-    $gestor = fopen($archivo_guardado, "r");
-        while ($datos = fgetcsv($gestor, 1000, ";"))
-        {
-          if($fila!=1)
-          {     
-            $newDate= date('Y-m-d',strtotime($datos[5]));
-            $import = "INSERT INTO alumnos (id_solucion,matricula,nombres,apellidos,usuario,nacimiento,genero,grado,seccion,correo,establecimiento,solucion) values ($datos[0],'$datos[1]','$datos[2]','$datos[3]','$datos[4]','$newDate','$datos[6]',$datos[7],'$datos[8]','$datos[9]','$datos[10]','$datos[11]')";
-            mysqli_query($conexion, $import); 
-
-          }
-          $fila++;  
-          
+  if (copy($archivo_copia,$archivo_guardado)){
+    echo ("Se copió exitosamente el archivo");
+    $csv = explode(".",$archivo_guardado);
+    if($csv[1]<>"csv"){
+      echo("Por favor elija un archivo con extensión CSV");
+    }else{
+      $data=array();
+      $fila = 0;
+      $gestor = fopen($archivo_guardado, "r");
+      while ($datos = fgetcsv($gestor, 0, ";"))
+      {
+        if($fila!=1)
+        {     
+          $newDate= date('Y-m-d',strtotime($datos[5]));
+          $import = "INSERT INTO alumnos (id_solucion,matricula,nombres,apellidos,usuario,nacimiento,genero,grado,seccion,correo,establecimiento,solucion) values ($datos[0],'$datos[1]','$datos[2]','$datos[3]','$datos[4]','$newDate','$datos[6]',$datos[7],'$datos[8]','$datos[9]','$datos[10]','$datos[11]')";
+          mysqli_query($conexion, $import); 
+        
         }
-    fclose($gestor);    
+         
+      }
+      $fila++; 
+      echo("Se importaron correctamente los datos");
+      fclose($gestor);
+    }
+  }else{
+    echo ("Se produjo un error al copiar el archivo");
   }
-
 }
 ?> 
  <!--Content-->
--- includes/subir_archivo.php
+++ includes/subir_archivo.php
@@ -1,20 +1,35 @@
 <?php
 require_once("../conexion/conexion.php");
 
-$ruta="includes/";
+ // $csv = explode(".",$archivo);
+  // if($csv[1]<>"csv"){
+  //   echo("Por favor elija un archivo con extensión CSV");
+  // }else{
+    $data=array();
+    $fila = 0;
+    $gestor = fopen($archivo_guardado, "r");
+    while ($datos = fgetcsv($gestor, 1000, ";"))
+    {
+      if($fila!=1)
+      {     
+        $newDate= date('Y-m-d',strtotime($datos[5]));
+        $import = "INSERT INTO alumnos (id_solucion,matricula,nombres,apellidos,usuario,nacimiento,genero,grado,seccion,correo,establecimiento,solucion) values ($datos[0],'$datos[1]','$datos[2]','$datos[3]','$datos[4]','$newDate','$datos[6]',$datos[7],'$datos[8]','$datos[9]','$datos[10]','$datos[11]')";
+        mysqli_query($conexion, $import); 
 
-foreach($_FILES as $key){
-    $nombre =$key["name"];
+      }
+      $fila++;  
+      
+    }
+      
+    echo("Se importaron correctamente los datos");
+    fclose($gestor);  
+#  }
 
-    echo ($nombre);
-
-    die();
-
-
-}
-
-
-
+  // if (copy($archivo_copia,$archivo_guardado)){
+  //   echo ("Se copió exitosamente el archivo");
+  // }else{
+  //   echo ("Se produjo un error al copiar el archivo");
+  // }
 
 
 ?>
